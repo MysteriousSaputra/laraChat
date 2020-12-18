0c6f29a90a5592e39421b9950e79ba1d18b8c0d2
il_ham <ilham05saputra@gmail.com>
2020-12-18 15:54:29 +0700

    update sc

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -6,8 +6,31 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <style>
+        .list-group{
+            overflow-y: scroll;
+            height:200px;
+            background-color: white;
+        }
+    </style>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row" id='app'>
+            <div class="offset-md-4 col-4">
+                <li class="list-group-item active">Chat Room</li>
+                <ul class="list-group">
+                    <message
+                    v-for='value in chat.message'
+                    v-bind:data="value"
+                    v-bind:key="value.text"
+                    >@{{value}}</message>
+                </ul>
+                <input type="text" class="form-control" placeholder="Type your message" v-model='message' @keyup.enter='send'>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('/js/app.js') }}"></script>
 </body>
 </html>
